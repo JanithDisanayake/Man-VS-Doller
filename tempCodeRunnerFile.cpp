203ca9104b845b46af68a495e293db81b70b0988
J004 <janith.kavinda3003@gmail.com>
2022-08-19 23:41:48 +0530

    show enemy rain fall in console

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+if (enemyCounter == 50)
+	// {
+	// 	// enemyX[enemyNumber] = rand() % width;
+	// 	// enemyY[enemyNumber] = rand() % height;
+	// 	enemyNumber++;
+	// 	if (enemyNumber == 5) enemyNumber = 0;
+	// 	enemyCounter = 0;
+	// }
