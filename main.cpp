465d5fde3cbf8e56e62d564c97e1f408c4f7f5e4
J004 <janith.kavinda3003@gmail.com>
2022-08-20 01:07:20 +0530

    show enemy rain fall with 2 enemies

--- main.cpp
+++ main.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 int height = 20;
 int width = 20;
-int enemyX;
-int enemyY = 1;
+int enemyX[5];
+int enemyY[5];
 int enemyCounter;
 int enemyNumber = 0;
+int enemyNumber2 = 1;
 bool gameOver = false;
 
 void draw() {
@@ -16,10 +17,8 @@ void draw() {
 	for (int i = 0; i < height; i++) {
 		for (int j = 0; j < width; j++) {
 			if (i == 0 || i == height - 1 || j == 0 || j == width - 1)	cout << "#";
-			else if (enemyX == j && enemyY ==i) {
-				cout << "*";
-			}
-			// else if (enemyX[0] == i && enemyY[0] == j) cout << "*";
+			else if (enemyX[0] == j && enemyY[0] ==i || enemyX[1] == j && enemyY[1] == i) cout << "*";
+			// else if () cout << "*";
 			// else if (enemyX[1] == i && enemyY[1] == j) cout << "*";
 			// else if (enemyX[2] == i && enemyY[2] == j) cout << "*";
 			// else if (enemyX[3] == i && enemyY[3] == j) cout << "*";
@@ -28,28 +27,32 @@ void draw() {
 		}
 		cout << "\n";
 	}
-	cout << enemyX;
-	enemyY++;
+	enemyY[0]++;
+	enemyY[1]++;
+	cout << enemyY[0];
+	cout << " ";
+	cout << enemyY[1];
 }
 
 void logic() {
 	
-	if (enemyCounter == width)
+	if (enemyCounter == 10) {
+		enemyX[enemyNumber2] = rand() % width;
+		enemyY[enemyNumber2++] = 1;
+		if (enemyNumber2>2) enemyNumber2 = 0;
+	} 
+	
+	if (enemyCounter == 20)
 	{
-		
-		enemyX = rand() % 20;
-		enemyY = 1;
-		// enemyX[enemyNumber] = rand() % width;
-		// enemyY[enemyNumber] = rand() % height;
-		enemyNumber++;
-		if (enemyNumber == 5) enemyNumber = 0;
+		enemyY[enemyNumber++]=1;
 		enemyCounter = 0;
+		if (enemyNumber>2) enemyNumber = 0;
 	}
 
 }
 
 void setup() {
-	enemyX = rand() % width + 1;
+	enemyX[0] = rand() % width;
 	cout << enemyX;
 }
 
