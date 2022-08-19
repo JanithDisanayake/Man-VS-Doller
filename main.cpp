7372753fc3a96f6007b4f0de6c7fd093e326ea76
J004 <janith.kavinda3003@gmail.com>
2022-08-19 21:52:41 +0530

    show random enemy on console

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int height = 20;
+int width = 20;
+int enemyX[5];
+int enemyY[5];
+int enemyCounter;
+int enemyNumber = 0;
+bool gameOver = false;
+
+void draw() {
+	system("cls");
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			if (i == 0 || i == height - 1 || j == 0 || j == width - 1)	cout << "#";
+			else if (enemyX[0] == i && enemyY[0] == j) cout << "*";
+			else if (enemyX[1] == i && enemyY[1] == j) cout << "*";
+			else if (enemyX[2] == i && enemyY[2] == j) cout << "*";
+			else if (enemyX[3] == i && enemyY[3] == j) cout << "*";
+			else if (enemyX[4] == i && enemyY[4] == j) cout << "*";
+			else cout << " ";
+		}
+		cout << "\n";
+	}
+}
+
+void logic() {
+	
+	if (enemyCounter == 50)
+	{
+		enemyX[enemyNumber] = rand() % width;
+		enemyY[enemyNumber] = rand() % height;
+		enemyNumber++;
+		if (enemyNumber == 5) enemyNumber = 0;
+		enemyCounter = 0;
+	}
+
+}
+
+int main() {
+
+	while(!gameOver)
+	{
+		draw();
+		logic();
+		enemyCounter++;
+	}
+}
