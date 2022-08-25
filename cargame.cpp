e411cda4353a773d3d3eff6dfe83e9b711bdfc6d
J004 <janith.kavinda3003@gmail.com>
2022-08-25 21:23:30 +0530

    doller logo succussfully executed in cargame

--- cargame.cpp
+++ cargame.cpp
@@ -17,7 +17,30 @@ enum Dir {STOP = 0, LEFT, RIGHT};
 Dir dir;
 
 
+void logoDoller() {
+    system("cls");
+
+    for(int p = 0; p < 20; p++) {
+		for(int q = 0; q<25; q++) {
+			if(p<18 && p>1 && (q==11 || q==12 || q==13))    cout<<" ";
+            else if(p==6 && q<20 && 5<=q) cout<<" ";
+            else if((p==7 && (q==4 || q==5))||
+                    (p==8 && (q==3 || q==4))||
+                    (p==9 && (q==4 || q==5))||
+                    (p==10 && q<20 && 5<=q)||
+                    (p==11 && (q==19 || q==20))||
+                    (p==12 && (q==20 || q==21))||
+                    (p==13 && (q==19 || q==20))) cout<<" ";
+            else if(p==14 && q<20 && 5<=q) cout<<" ";
+			else cout<<"#";
+		}
+		cout<<"\n";
+	}
+}
+
 void setup() {
+	logoDoller();
+    Sleep(10000);
 	carX = width/2;
     carY = height-2;
 }
@@ -103,26 +126,5 @@ int main() {
 		logic();
 		enemyCounter+=1;
 		Sleep(100);
-	}
-}
-
-void logoDoller() {
-    system("cls");
-
-    for(int p = 0; p < 20; p++) {
-		for(int q = 0; q<25; q++) {
-			if(p<18 && p>1 && (q==11 || q==12 || q==13))    cout<<" ";
-            else if(p==6 && q<20 && 5<=q) cout<<" ";
-            else if((p==7 && (q==4 || q==5))||
-                    (p==8 && (q==3 || q==4))||
-                    (p==9 && (q==4 || q==5))||
-                    (p==10 && q<20 && 5<=q)||
-                    (p==11 && (q==19 || q==20))||
-                    (p==12 && (q==20 || q==21))||
-                    (p==13 && (q==19 || q==20))) cout<<" ";
-            else if(p==14 && q<20 && 5<=q) cout<<" ";
-			else cout<<"#";
-		}
-		cout<<"\n";
 	}
 }
